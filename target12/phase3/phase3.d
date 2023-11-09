4a47e85f4d17df830cd3eff39488c106b4ae8b78
pedroazeredo04 <pedro_azeredo@usp.br>
2023-11-09 15:06:53 -0300

    ✨ Add phase3

---
+++ target12/phase3/phase3.d
@@ -0,0 +1,9 @@
+
+phase3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 98 74 61 55 	mov    $0x55617498,%rdi
+   7:	c3                   	ret    
---
+++ target12/phase3/phase3.s
@@ -0,0 +1,2 @@
+movq $0x55617498, %rdi /* %rsp + 0x18 */
+retq
